39af7e324bdd3cd19727ba3824e588a255b13bee
Kipras Melnikovas <kipras@kipras.org>
2019-12-07 14:32:36 +0200

    Pridėt komentarus paaiškinimui
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- blink-keitinejant/blink-keitinejant.ino
+++ blink-keitinejant/blink-keitinejant.ino
@@ -12,16 +12,33 @@ byte kelintasIjungtas = 0;
 byte dabartineBusena = -1;
 
 void ijungtiKitaLeda() {
+	/** išjungti seną */
 	digitalWrite(leduPinai[kelintasIjungtas], LOW);
+
+	/**
+	 * pasirinkti kitą piną įjungimui
+	 * (liekana tam, kad pasiekus paskutinį piną vėl eitume nuo pradžios)
+	*/
 	kelintasIjungtas = (kelintasIjungtas + 1) % leduSkaicius;
-	Serial.println(kelintasIjungtas);
+
+	/**
+	 * įjungiam kitą
+	 */
 	digitalWrite(leduPinai[kelintasIjungtas], HIGH);
+
+	/**
+	 * printinam į serial monitorių (šiaip sau),
+	 * kuris dabar įjungtas
+	 */
+	Serial.println(kelintasIjungtas);
 }
 
 void setup() {
 	Serial.begin(9600);
+
 	pinMode(mygtukoPinas, INPUT);
 
+	/** priskirti visus ledus OUTPUT'ui */
 	for (size_t i = 0; i < leduSkaicius; ++i) {
 		pinMode(leduPinai[i], OUTPUT);
 	}
